32912a2c9114e8d675a44e5e1fcf8b66c1c748fd
manuthecoder <manuthecoder@protonmail.com>
2022-07-19 19:50:12 +0000

    E

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import TagManager from "react-gtm-module";
-
-TagManager.initialize({
-  gtmId: "GTM-PPH4TH4",
-});
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PPH4TH4');`,
+          }}
+        />
         <title>
           Smartlist - sophisticated home and business inventory management for
           free.
@@ -67,6 +71,11 @@ export default function Document() {
         />
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PPH4TH4" height="0" width="0" style="display: none; visibility: hidden;" />`,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
