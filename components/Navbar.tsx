25663cd96429b01d1eefc9bf46542ff70aeecc1c
manuthecoder <manuthecoder@protonmail.com>
2022-08-15 20:57:41 +0000

    update 2 files and delete 1 file

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,7 +35,7 @@ const solutions = [
 ];
 const callsToAction = [
   { name: "Help", href: "#", icon: PlayIcon },
-  { name: "Official twitter", href: "#", icon: Twitter },
+  { name: <>Official&nbsp;twitter</>, href: "#", icon: Twitter },
   { name: "Discord", href: "#", icon: MessageSquare },
 ];
 const resources = [
@@ -89,14 +89,12 @@ export default function Example() {
           <a href="#" className="flex items-center gap-5">
             <img
               className="h-8 w-auto sm:h-10"
-              src="https://tailwindui.com/img/logos/workflow-mark.svg?color=teal&shade=600"
+              src="https://cdn.jsdelivr.net/gh/Smartlist-App/Assets@master/v2/rounded.png"
               alt=""
             />
-            <span className="text-2xl">Smartlist</span>
           </a>
         </div>
         <Popover.Group as="nav" className="hidden md:flex space-x-10">
-          {" "}
           <a
             href="#"
             className="text-base font-medium text-gray-500 hover:text-gray-900"
@@ -162,8 +160,8 @@ export default function Example() {
                         ))}
                       </div>
                       <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                        {callsToAction.map((item) => (
-                          <div key={item.name} className="flow-root">
+                        {callsToAction.map((item, key) => (
+                          <div key={key.toString()} className="flow-root">
                             <a
                               href={item.href}
                               className="-m-3 p-3 flex items-center rounded-xl text-base font-medium text-gray-900 hover:bg-gray-100"
@@ -193,7 +191,7 @@ export default function Example() {
           </a>
           <a
             href="#"
-            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-teal-600 hover:bg-teal-700"
+            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 rounded-2xl shadow-sm text-base font-medium text-white bg-gradient-to-br from-orange-400 to-orange-900"
           >
             Get started
           </a>
