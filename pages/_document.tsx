d8e67c92ffa35ef38da880406d5c02c2b4bbba12
manuthecoder <manuthecoder@protonmail.com>
2022-08-17 17:37:39 +0000

    update 404.tsx and _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,6 +29,14 @@ export default function Document() {
          `}
       </Script>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PPH4TH4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
