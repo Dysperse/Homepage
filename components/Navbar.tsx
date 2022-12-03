e5ad98d32bf17c7b2318ed6975d9cce370a8a35a
_manu.codes <manuthecoder@protonmail.com>
2022-12-03 04:41:14 +0000

    update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -138,24 +138,24 @@ export default function Example() {
 
                 <Transition
                   as={Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="opacity-0 translate-y-1"
-                  enterTo="opacity-100 translate-y-0"
+                  enter="transition ease-out duration-150"
+                  enterFrom="translate-y-1"
+                  enterTo="translate-y-0"
                   leave="transition ease-in duration-150"
-                  leaveFrom="opacity-100 translate-y-0"
-                  leaveTo="opacity-0 translate-y-1"
+                  leaveFrom="translate-y-0"
+                  leaveTo="translate-y-1"
                 >
                   <Popover.Panel className="absolute z-10 fixed right-0 mt-3 px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                    <div className="rounded-lg shadow-lg ring-1 ring-neutral-200 overflow-hidden">
-                      <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                    <div className="rounded-lg shadow-lg  overflow-hidden">
+                      <div className="relative grid gap-6 bg-neutral-100 px-5 py-6 sm:gap-8 sm:p-8">
                         {solutions.map((item) => (
                           <a
                             key={item.name}
                             href={item.href}
-                            className="-m-3 p-3 flex items-start rounded-2xl hover:bg-neutral-50"
+                            className="-m-3 p-3 flex items-start rounded-2xl hover:bg-neutral-200"
                           >
                             <item.icon
-                              className="flex-shrink-0 h-6 w-6 mt-1 text-teal-600"
+                              className="flex-shrink-0 h-6 w-6 mt-1 text-neutral-600"
                               aria-hidden="true"
                             />
                             <div className="ml-4">
@@ -169,7 +169,7 @@ export default function Example() {
                           </a>
                         ))}
                       </div>
-                      <div className="px-5 py-5 bg-neutral-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+                      <div className="px-5 py-5 bg-neutral-200 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
                         Suggestions? Contact us at:&nbsp;
                         <a
                           href="mailto:hello@smartlist.tech"
