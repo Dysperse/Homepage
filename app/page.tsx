55f65456e595adbd774fcdf6bee3dc1d81c6a330
_manu.codes <manuthecoder@protonmail.com>
2023-05-10 05:06:24 +0000

    redesign?

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Box, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box>
+      <Typography variant="h1">Hello, world!</Typography>
+    </Box>
+  );
+}
