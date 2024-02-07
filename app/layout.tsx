3c0ea3b6a32a1c6a5771afbd684a6f3f03a7217e
Manu G <manusvathgurudath@gmail.com>
2024-02-07 14:54:12 +0530

    W

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
         <link rel="icon" href="/favicon.ico" />
       </head>
--- app/page.tsx
+++ app/page.tsx
@@ -1,87 +1,247 @@
-import { Box, Button, Chip, Typography } from "@mui/material";
+import { Box, Button, Chip, Link, SxProps, Typography } from "@mui/material";
 import { mintDark } from "./themes";
 import Image from "next/image";
-import { Jost } from "next/font/google";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import { Jost, JetBrains_Mono } from "next/font/google";
 
 const jost = Jost({ subsets: ["latin"] });
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: "variable",
+});
 
-export default function Page() {
+function Emoji({
+  size = 24,
+  emoji,
+  style = {},
+}: {
+  size?: number;
+  emoji: string;
+  style?: any;
+}) {
   return (
-    <Box
-      sx={{
-        bgcolor: mintDark.mint11,
-        height: "100vh",
-        width: "100vw",
-        padding: "3px",
-        userSelect: "none",
-        boxSizing: "border-box",
-        color: mintDark.mint12,
+    <img
+      style={{
+        width: size,
+        height: size,
       }}
+      alt="Emoji"
+      src={`https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/${emoji?.toLowerCase()}.png`}
+    />
+  );
+}
+
+const CardContainer = ({
+  children,
+  sx,
+}: {
+  children: React.ReactNode;
+  sx?: SxProps;
+}) => (
+  <Box
+    sx={{
+      bgcolor: mintDark.mint2,
+      display: "flex",
+      px: 5,
+      flex: 1,
+      flexDirection: "column",
+      justifyContent: "center",
+      borderRadius: 7,
+      border: `1px solid ${mintDark.mint5}`,
+      "& *": {
+        fontFamily: jost.style.fontFamily,
+      },
+      ...sx,
+    }}
+  >
+    {children}
+  </Box>
+);
+
+const Familiar = () => (
+  <CardContainer sx={{ flex: 2 }}>
+    <Typography
+      className={jetBrainsMono.className}
+      fontSize={12}
+      fontWeight={900}
     >
+      Familiar.
+    </Typography>
+    <Typography className={jost.className} fontWeight={200}>
+      Like your everyday browser, but for navigating <i>productivity</i>.
+    </Typography>
+  </CardContainer>
+);
+
+const Synced = () => (
+  <CardContainer>
+    <Typography
+      className={jetBrainsMono.className}
+      fontSize={12}
+      fontWeight={900}
+    >
+      Synced.
+    </Typography>
+    <Typography className={jost.className} fontWeight={200}>
+      Always up to date, no matter where you are.
+    </Typography>
+  </CardContainer>
+);
+const Flexible = () => {
+  const options = [
+    { icon: "calendar_today", text: "Agenda" },
+    { icon: "view_kanban", text: "Kanban" },
+    { icon: "view_agenda", text: "Stream" },
+    { icon: "view_cozy", text: "Grid" },
+    { icon: "exercise", text: "Workload" },
+  ];
+  return (
+    <CardContainer>
+      <Typography
+        className={jetBrainsMono.className}
+        fontSize={16}
+        fontWeight={900}
+      >
+        Flexible.
+      </Typography>
+      <Typography className={jost.className} fontWeight={200}>
+        Want precision? Or a more relaxed view? Maybe both? We got you.
+      </Typography>
       <Box
         sx={{
-          bgcolor: mintDark.mint2,
-          minHeight: "100%",
           display: "flex",
-          px: 5,
-          flexDirection: "column",
-          justifyContent: "center",
-          borderRadius: 10,
-          alignItems: { sm: "center" },
-          "& *": {
-            fontFamily: jost.style.fontFamily,
-          },
+          gap: 1,
+          mt: 1,
+          flexWrap: "wrap",
         }}
       >
-        <Image
-          width={70}
-          height={70}
-          src="/logo.svg"
-          alt="logo"
-          style={{
-            marginTop: 20,
-            borderRadius: 99,
-            marginBottom: "auto",
-            alignSelf: "center",
-          }}
-        />
-        <Typography
-          className={jost.className}
-          sx={{
-            fontWeight: 300,
-            marginBottom: 2,
-            opacity: 0.6,
-          }}
-        >
-          coming spring 2024
-        </Typography>
-        <Typography
-          className={jost.className}
-          sx={{
-            textAlign: { sm: "center" },
-            fontWeight: 900,
-            fontSize: { xs: 30, sm: 50, md: 70 },
-            "& span": {
-              color: mintDark.mint12,
-            },
-          }}
-          variant="h2"
-        >
-          productivity for <span>humans</span>.
-        </Typography>
-        <Typography
-          className={jost.className}
-          variant="h5"
-          sx={{
-            textAlign: { sm: "center" },
-            fontWeight: 200,
-            marginTop: 2,
-            fontSize: { xs: 16, sm: 30 },
-          }}
-        >
-          we're not just building a product, we're building a movement.
-        </Typography>
-        {/* <Button
+        {options.map((option) => (
+          <Box key={option.text}>
+            <Chip
+              label={option.text}
+              icon={
+                <span
+                  style={{ color: mintDark.mint11 }}
+                  className="material-symbols-rounded"
+                >
+                  {option.icon}
+                </span>
+              }
+              sx={{
+                color: mintDark.mint11,
+                bgcolor: mintDark.mint4,
+                borderRadius: 99,
+              }}
+            />
+          </Box>
+        ))}
+      </Box>
+    </CardContainer>
+  );
+};
+
+const Sophisticated = () => (
+  <CardContainer>
+    <Typography
+      className={jetBrainsMono.className}
+      fontSize={12}
+      fontWeight={900}
+    >
+      Sophisticated.
+    </Typography>
+    <Typography className={jost.className} fontWeight={200}>
+      Integrated AI which understands you.
+    </Typography>
+  </CardContainer>
+);
+
+const Integrated = () => (
+  <CardContainer>
+    <Typography
+      className={jetBrainsMono.className}
+      fontSize={12}
+      fontWeight={900}
+    >
+      Integrated.
+    </Typography>
+    <Typography className={jost.className} fontWeight={200}>
+      Connects with your favorite apps.
+    </Typography>
+  </CardContainer>
+);
+
+const Customizable = () => (
+  <CardContainer>
+    <Typography
+      className={jetBrainsMono.className}
+      fontSize={12}
+      fontWeight={900}
+    >
+      Customizable.
+    </Typography>
+    <Typography className={jost.className} fontWeight={200}>
+      Make it your own with themes, emojis, & more.
+    </Typography>
+  </CardContainer>
+);
+
+function About() {
+  return (
+    <CardContainer>
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width={70}
+        height={70}
+        style={{
+          alignSelf: "center",
+          marginTop: 20,
+          marginBottom: "auto",
+        }}
+      />
+
+      <Typography
+        className={jetBrainsMono.className}
+        sx={{
+          mt: -2,
+          fontWeight: 300,
+          opacity: 0.6,
+        }}
+      >
+        arriving spring 2024
+      </Typography>
+      <Typography
+        className={jost.className}
+        sx={{
+          fontWeight: 900,
+          fontSize: { xs: 30, sm: 50, md: 70 },
+          "& .humans": {
+            color: mintDark.mint11,
+          },
+          "& .productivity": {
+            fontWeight: 100,
+          },
+        }}
+        variant="h2"
+      >
+        <span className="productivity">productivity&nbsp;for</span>
+        <br />
+        <span className="humans">humans</span>
+      </Typography>
+      <Typography
+        className={jost.className}
+        variant="h5"
+        sx={{
+          mt: 1,
+          fontWeight: 400,
+          fontSize: { xs: 16, sm: 30 },
+          opacity: 0.3,
+        }}
+      >
+        #dysperse makes organization simple again
+      </Typography>
+      <Box sx={{ gap: 2, display: "flex", flexDirection: "row" }}>
+        <Button
           sx={{
             marginTop: 3,
             background: mintDark.mint10,
@@ -95,18 +255,19 @@ export default function Page() {
             textTransform: "none",
             borderRadius: 99,
             transition: "none",
-            gap: 2,
+            gap: 1,
           }}
           disableElevation
           disableRipple
           size="large"
           variant="contained"
           className={jost.className}
-          href="https://my.dysperse.com"
+          href="https://app.dysperse.com/auth/sign-up"
           target="_blank"
         >
-          Induct myself <span className="material-symbols-outlined">east</span>
-        </Button> */}
+          Induct myself{" "}
+          <span className="material-symbols-rounded">north_east</span>
+        </Button>
         <Button
           sx={{
             marginTop: 3,
@@ -121,18 +282,105 @@ export default function Page() {
             textTransform: "none",
             borderRadius: 99,
             transition: "none",
-            gap: 2,
+            gap: 1,
           }}
           disableElevation
           disableRipple
           size="large"
           className={jost.className}
-          href="https://my.dysperse.com"
+          href="https://app.dysperse.com/auth/sign-in"
           target="_blank"
         >
-          Visit old site
+          Login
         </Button>
-        <Box sx={{ marginBottom: "auto" }} />
+      </Box>
+      <Box sx={{ marginBottom: "auto" }} />
+    </CardContainer>
+  );
+}
+
+export default function Page() {
+  return (
+    <Box
+      sx={{
+        maxWidth: "100vw",
+        color: mintDark.mint12,
+        display: "flex",
+        userSelect: "none",
+        flexDirection: "column",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          p: 2,
+          gap: 2,
+          flexDirection: "row",
+          height: "100dvh",
+          boxSizing: "border-box",
+        }}
+      >
+        <Box sx={{ width: "500px", height: "100%", display: "flex" }}>
+          <About />
+        </Box>
+        <Box
+          sx={{
+            gap: 2,
+            flex: 1,
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Box sx={{ gap: 2, display: "flex", flex: 1 }}>
+            <Familiar />
+            <Sophisticated />
+            <Synced />
+          </Box>
+          <Box sx={{ gap: 2, display: "flex", flex: 2 }}>
+            <Box
+              sx={{ flex: 3, display: "flex", flexDirection: "column", gap: 2 }}
+            >
+              <Flexible />
+              <Integrated />
+            </Box>
+            <Box
+              sx={{ flex: 2, display: "flex", flexDirection: "column", gap: 2 }}
+            >
+              <Customizable />
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          borderTop: `1px solid ${mintDark.mint5}`,
+          backdropFilter: "blur(10px)",
+          py: 5,
+          px: 10,
+
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex",
+          gap: 2,
+          "& .MuiLink-root": {
+            color: mintDark.mint8,
+            textDecorationColor: mintDark.mint6,
+            "&:hover": {
+              color: mintDark.mint10,
+              textDecorationColor: mintDark.mint8,
+            },
+          },
+        }}
+      >
+        <Link href="/" sx={{ mr: "auto" }}>
+          What's new
+        </Link>
+
+        <Link href="/">Status</Link>
+        <Link href="/">Blog</Link>
+        <Link href="/">Terms</Link>
+        <Link href="/">Privacy Policy</Link>
       </Box>
     </Box>
   );
