0d3b0b0b5daf9e910b13e87861d48272c51295cc
Manu G <manusvathgurudath@gmail.com>
2024-05-03 16:52:47 +0000

    W

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "api.producthunt.com",
+          port: "",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "cdn.jsdelivr.net",
+          port: "",
+          pathname: "/**",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
