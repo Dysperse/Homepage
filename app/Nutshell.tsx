f224676b27d8803a10d973aa151a2e63aad644be
Manu G <manusvathgurudath@gmail.com>
2025-01-17 22:35:10 -0800

    Add Dialog component and integrate with Nutshell; update styles and dependencies

--- app/Nutshell.tsx
+++ app/Nutshell.tsx
@@ -3,6 +3,14 @@ import type { ReactNode } from "react";
 import { Bricolage_Grotesque } from "next/font/google";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const bricolage = Bricolage_Grotesque({
   subsets: ["latin"],
@@ -71,23 +79,120 @@ export const Nutshell = () => {
           <div className="shrink-0 w-10 bg-gradient-to-r h-60 from-white to-transparent z-10 -ml-10 px-5 py-5 -mr-10"></div>
           <div className="flex gap-2 overflow-x-auto px-10">
             {[
-              { title: "List", icon: "list" },
-              { title: "Kanban", icon: "grid" },
-              { title: "Grid", icon: "calendar" },
-              { title: "Planner", icon: "kanban" },
-              { title: "Skyline", icon: "table" },
-              { title: "Stream", icon: "timeline" },
-              { title: "Workload", icon: "timeline" },
-              { title: "Matrix", icon: "map" },
-              { title: "Calendar", icon: "gallery" },
+              {
+                title: "List",
+                icon: "view_agenda",
+                description:
+                  "The good old trusty to-do list. Perfect for simple task management and keeping track of your daily activities.",
+              },
+              {
+                title: "Kanban",
+                icon: "view_kanban",
+                description:
+                  "Organize tasks by category. Ideal for project management and visualizing your workflow.",
+              },
+              {
+                title: "Grid",
+                icon: "view_cozy",
+                description:
+                  "Organize tasks in a grid by category. Great for a more structured and compartmentalized view of your tasks.",
+              },
+              {
+                title: "Planner",
+                icon: "transition_slide",
+                description:
+                  "View tasks day by day. Excellent for daily planning and ensuring you stay on top of your schedule.",
+              },
+              {
+                title: "Skyline",
+                icon: "blur_linear",
+                description:
+                  "Vision tasks in flexible time frames. Useful for long-term planning and seeing the bigger picture.",
+              },
+              {
+                title: "Stream",
+                icon: "whatshot",
+                description:
+                  "See missed, upcoming, and completed tasks. Helps you track your progress and stay motivated.",
+              },
+              {
+                title: "Workload",
+                icon: "exercise",
+                description:
+                  "Sort tasks by an estimated difficulty. Useful for balancing your workload and avoiding burnout.",
+              },
+              {
+                title: "Matrix",
+                icon: "target",
+                description:
+                  "Prioritize tasks by urgency and importance. Helps you focus on what matters most and manage your time effectively.",
+              },
+              {
+                title: "Calendar",
+                icon: "calendar_today",
+                description:
+                  "The good old traditional calendar view. Perfect for scheduling and keeping track of important dates and deadlines.",
+              },
             ].map((_, i) => (
-              <div className="shrink-0 w-40 h-60 px-5 py-5 rounded-3xl relative bg-gray-100 overflow-hidden">
-                <Image alt={`View ${i + 1}`} src={`/views/${i + 1}.png`} fill />
+              <Dialog
+                key={i}
+                onOpenChange={(t) => {
+                  if (t)
+                    document.documentElement.classList.add("scrollbar-hidden");
+                  else
+                    document.documentElement.classList.remove(
+                      "scrollbar-hidden"
+                    );
+                }}
+              >
+                <DialogTrigger>
+                  <div className="cursor-pointer active:scale-95 duration-75 transition-transform shrink-0 w-40 h-60 px-5 py-5 rounded-3xl relative bg-gray-100 overflow-hidden">
+                    <Image
+                      alt={`View ${i + 1}`}
+                      src={`/views/${i + 1}.png`}
+                      fill
+                    />
 
-                <div className="absolute inset-0 bg-gradient-to-b flex items-end font-black p-3 text-lg text-white from-neutral-100/5 to-neutral-900/40">
-                  <h3>{_.title} </h3>
-                </div>
-              </div>
+                    <div
+                      className="absolute inset-0 bg-gradient-to-b flex items-end font-medium p-3 text-2xl text-white from-neutral-100/5 to-neutral-900/50"
+                      style={bricolage.style}
+                    >
+                      <h3>{_.title} </h3>
+                    </div>
+                  </div>
+                </DialogTrigger>
+                <DialogContent className="max-w-2xl grid grid-cols-2 p-0 overflow-hidden border-none">
+                  <div
+                    className="relative"
+                    style={{ aspectRatio: "160/240", maxHeight: "60vh" }}
+                  >
+                    <Image
+                      alt={`View ${i + 1}`}
+                      src={`/views/${i + 1}.png`}
+                      fill
+                    />
+                  </div>
+                  <div className="flex flex-col gap-3 py-5 pt-10">
+                    <span
+                      className="material-symbols-rounded mt-4"
+                      style={{ fontSize: 40 }}
+                    >
+                      {_.icon}
+                    </span>
+                    <DialogTitle className="text-4xl mt-2 font-bold text-neutral-700 dark:text-neutral-50">
+                      {_.title}
+                    </DialogTitle>
+                    <p className="text-2xl font-thin pr-12 text-pretty">
+                      {_.description}
+                    </p>
+                    <div className="flex mt-auto flex-col pr-12">
+                      <Button className="h-12 rounded-3xl text-lg">
+                        Find {_.title.toLowerCase()} templates
+                      </Button>
+                    </div>
+                  </div>
+                </DialogContent>
+              </Dialog>
             ))}
           </div>
           <div className="shrink-0 w-10 bg-gradient-to-l h-60 from-white to-transparent z-10 -ml-10 px-5 py-5"></div>
